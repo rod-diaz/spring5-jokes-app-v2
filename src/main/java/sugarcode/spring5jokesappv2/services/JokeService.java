da0e0a84099aeeb796aa7d237c76cbbc4430773a
Rodrigo Diaz <rod_diaz@outlook.com>
2021-05-27 13:48:09 -0500

    Add Joke Service

---
+++ src/main/java/sugarcode/spring5jokesappv2/services/JokeService.java
@@ -0,0 +1,9 @@
+package sugarcode.spring5jokesappv2.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+public interface JokeService {
+
+    String getJoke();
+}
